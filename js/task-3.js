cddb6e0545401eca8b0ca4c2d754ba5712fe7a04
Mariya Buzhava <bujava.masha@gmail.com>
2024-05-30 22:32:18 +0300

    update

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,15 @@
-function checkForSpam(message) {
-
-    const blaclistedWord1 = "spam";
-    const blaclistedWord2 = "sale";
-    const messageLowerCase = message.toLowerCase();
-    
-    const result = messageLowerCase.includes(blaclistedWord1) || messageLowerCase.includes(blaclistedWord2);
-
-    return result;
-
+function filterArray(numbers, value) {
+    const array = [];
+    for (let i = 0; i < numbers.length; i += 1) {
+        if (numbers[i] > value) {
+            array.push(numbers[i])
+        }
+    }
+    return array
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
